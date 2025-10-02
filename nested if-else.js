26a39b51d462f7b31637b36bbd8407b04e8fe590
mdsharia4613 <mdsharia4613@gmail.com>
2025-10-02 15:48:09 +0600

    nested provlem solving

--- nested if-else.js
+++ nested if-else.js
@@ -1,4 +1,4 @@
-
+// problem-1
 const user = {
     name: 'Sharia',
     active: true,
@@ -6,55 +6,90 @@ const user = {
     features: false,
 }
 
-if (user.active) {
-    if (user.subscription === "premium") {
-        console.log("wolcome to my page");
-        if (user.features === true) {
-            console.log("img, video")
-        }
-        else {
-            console.log("free dekho er na hoi premium neo")
-        }
-    }
-    else {
-        console.log("free version used koro")
-    }
+// if (user.active) {
+//     if (user.subscription === "premium") {
+//         console.log("wolcome to my page");
+//         if (user.features === true) {
+//             console.log("img, video")
+//         }
+//         else {
+//             console.log("free dekho er na hoi premium neo")
+//         }
+//     }
+//     else {
+//         console.log("free version used koro")
+//     }
 
+// }
+// else {
+//     console.log("your account is not active")
+// }
+
+// provlem-2
+// const guest ={
+//     tea: false,
+//     biscuit: false,
+// }
+
+// if(guest.tea === true){
+//     if(guest.biscuit === true){
+//         console.log("tea and biscuit khabo")
+//     }
+//     else{
+//         console.log('sodo tea khabo')
+//     }
+// }
+// else{
+//     console.log('tv dekho')
+// }
+
+// provlem-3
+
+const ref ={
+    refFood: false,
+    foodOder: false,
 }
-else {
-    console.log("your account is not active")
+if(ref.refFood === true){
+    console.log("food grom korbo")
+    if(ref.foodOder === true){
+        console.log('oder-foood')
+    }
+    
+}
+else{
+    console.log('aj roja thekbo')
 }
 
 // function uesed kore nested if-else
 
     
-const users = {
-    name: 'Sharia',
-    active: true,
-    subscription: 'premium',
-    features: false,
-}
- function cheeckUserAccess(users){
-    if(users.active){
-        if(users.subscription === 'premium'){
-            console.log('wolcome to my page')
-            showPremiumFeatures()
-        }
-        else{
-            console.log('ueed to free version')
-            showFreeFeaturs()
-        }
-    }
-    else{
-        console.log('please open in your account')
-    }
- }
+// const users = {
+//     name: 'Sharia',
+//     active: true,
+//     subscription: 'premium',
+//     features: false,
+// }
+//  function cheeckUserAccess(users){
+//     if(users.active){
+//         if(users.subscription === 'premium'){
+//             console.log('wolcome to my page')
+//             showPremiumFeatures()
+//         }
+//         else{
+//             console.log('ueed to free version')
+//             showFreeFeaturs()
+//         }
+//     }
+//     else{
+//         console.log('please open in your account')
+//     }
+//  }
 
- function showPremiumFeatures(){
-    console.log('premium version')
- }
+//  function showPremiumFeatures(){
+//     console.log('premium version')
+//  }
 
- function showFreeFeaturs(){
-    console.log("free version dekho")
- }
- cheeckUserAccess(users);
+//  function showFreeFeaturs(){
+//     console.log("free version dekho")
+//  }
+//  cheeckUserAccess(users);
