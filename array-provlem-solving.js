38e82279069ae7caefe70c7452bb88a9bf0beff2
mdsharia4613 <mdsharia4613@gmail.com>
2025-10-02 16:21:08 +0600

    basic array provlem solving

---
+++ array-provlem-solving.js
@@ -0,0 +1,25 @@
+
+// array make kora
+
+const numbers =[1,2,3,4,5,6,6,78,];
+console.log(numbers.length)
+
+// array value exchange kora
+
+const number = [22, 33, 55,66,77]
+number[0] = 100;
+console.log(number)
+
+// array valu update kora
+// push array
+
+const value = [12, 22,44, 56]
+value.push(8,9,25)
+console.log(value)
+
+// array valu delate kora
+// push array
+
+const words = [12, 22,44, 56, 45, 78]
+words.pop();
+console.log(words)
