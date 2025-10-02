de8abd4edf7ee1faef59dbcf64b0ef244d329f20
mdsharia4613 <mdsharia4613@gmail.com>
2025-10-02 16:04:07 +0600

    else-if provlem solving

---
+++ else-if.js
@@ -0,0 +1,10 @@
+const temperature = -10;
+
+if(temperature < 0){
+    console.log('baby ice ice')
+}else if(temperature < 20 ){
+    console.log('baby cole cole')
+}
+else{
+    console.log('baby hot hot')
+}
--- if-else.js
+++ if-else.js
@@ -1,11 +1,20 @@
 
-const money = 1200;
-// grather Than
-if (money > 1000) {
-    console.log('going to boundor bari')
-}
-else {
-    console.log('going to nija bari')
-}
+// const money = 1200;
+// // grather Than
+// if (money > 1000) {
+//     console.log('going to boundor bari')
+// }
+// else {
+//     console.log('going to nija bari')
+// }
 
 
+const price = 1300;
+const copon = true;
+
+if(price > 1000 && copon == true){
+    console.log("shirt kinbo")
+}
+else{
+    console.log("bari chole asbo")
+}
