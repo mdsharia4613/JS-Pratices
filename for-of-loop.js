44b8c7bd6c8f379bb7a370b4a34ac84f94eff640
mdsharia4613 <mdsharia4613@gmail.com>
2025-10-02 17:40:17 +0600

    for loop pratices

---
+++ for-of-loop.js
@@ -0,0 +1,14 @@
+ const friends = ['asjik', 'asfs', 'Sfdgw', 'ertga']
+
+ for(const friend of friends){
+    console.log(friend)
+ }
+
+ let sum = 10
+ for(let i = 0; i <= 20; i = i + 2){
+    // console.log(i)
+    sum = sum + i;
+    //  console.log(sum)
+ }
+
+  console.log( "sum of",sum)
