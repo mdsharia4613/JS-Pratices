aadb5bde3612f0b61a085e72b4ace7551d2e028f
mdsharia4613 <mdsharia4613@gmail.com>
2025-10-03 20:49:51 +0600

    object pratices

--- object.js
+++ object.js
@@ -16,3 +16,46 @@ for( const num of Object.values(numbers)){
     n = n + num
 }
 console.log(n)
+
+const player ={
+    name : 'Mesi',
+    team: 'Argentina',
+    goals: 9
+}
+
+console.log(Object.values(player))
+console.log(Object.keys(player))
+
+const phones ={
+    brand:'Samsung',
+    model:'Galaxy S21',
+    price: 333323,
+
+}
+
+for(const phone of Object.keys(phones)){
+    console.log(phone)
+}
+
+// object freeze
+const adminUser ={
+    username:"mdsharia",
+    email: "mdsharia2312",
+    password: 1222,
+    role: "teacher"
+}
+
+Object.freeze(adminUser);
+adminUser.role = 'enginner'
+adminUser.email = 'mdabir34@gmaail.com'
+console.log(adminUser)
+
+const food ={
+    name: 'Pizza',
+    price: '500',
+    size: 'xl'
+}
+Object.seal(food)
+food.price = "1000";
+food.size = 'sm';
+console.log(food)
