3d97aa8d4046a854273bee2cc5b25e03e2371a0c
mdsharia4613 <mdsharia4613@gmail.com>
2025-10-09 00:23:17 +0600

    function pratices

---
+++ function3.js
@@ -0,0 +1,34 @@
+
+// function sumOfNumber(numbers){
+//     let sum = 0;
+//     for(const num of numbers){
+//         sum += num;
+//     }
+//     return sum
+// }
+// const nums = [10, 20, 230,]
+// console.log(sumOfNumber(nums))
+
+// function numberOfModoules(numbers){
+//     let sum = 0;
+//     for(const num of numbers){
+//         if(num % 2 == 0){
+//             sum += num;
+//         }
+//     }
+//     return sum;
+// }
+// const value = [2,4,6,7,59,12]
+// console.log(numberOfModoules(value));
+
+// function leapYear(year){
+//     if(year %400 == 0 || year % 4 == 0){
+//         return true
+//     }
+//     else if(year % 400 !== 0 || year % 100 == 0){
+//         return false;
+//     }
+
+// }
+// const values = 2052;
+// console.log(leapYear('leap year', values));
