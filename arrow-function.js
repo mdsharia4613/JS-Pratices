20b42038b86aa4ab3321c3711988fe3d73419ca3
mdsharia4613 <mdsharia4613@gmail.com>
2025-10-09 09:10:28 +0600

    arrow functoin paratices

---
+++ arrow-function.js
@@ -0,0 +1,16 @@
+// const doMath = (a,b,c) => {
+//     const sum = a + b + c;
+//     const sub = a-b-c;
+//     const result = sum + sub;
+//     return result;
+// };
+// const sum = doMath(10,20,30);
+// console.log(sum)
+
+// const index = (arr) => arr[0];
+// const value = index([10,23,34,55,6])
+// console.log(value) 
+
+// const multipullNumber = (x,y,z) => x*y*z;
+// const arrowNumner = multipullNumber(22, 34, 56)
+// console.log(arrowNumner);
