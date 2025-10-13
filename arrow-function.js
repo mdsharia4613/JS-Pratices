63d8185f877e39335e8a49ba9ae819394584c029
mdsharia4613 <mdsharia4613@gmail.com>
2025-10-14 01:59:37 +0600

    provlem pratices

--- arrow-function.js
+++ arrow-function.js
@@ -7,6 +7,8 @@
 // const sum = doMath(10,20,30);
 // console.log(sum)
 
+
+
 // const index = (arr) => arr[0];
 // const value = index([10,23,34,55,6])
 // console.log(value) 
@@ -14,3 +16,14 @@
 // const multipullNumber = (x,y,z) => x*y*z;
 // const arrowNumner = multipullNumber(22, 34, 56)
 // console.log(arrowNumner);
+
+
+// const arr = () => 'unknown'
+// console.log(arr())
+
+const user ={
+    name : 'ashik',
+    taka: 34,
+}
+const people = (obj) => obj.taka % 5;
+console.log(people(user))
